bab77d24d1ecbf7744dcd78666e4278a329f7d3a
Rashmi Konaraddi <rashmikonaraddi@gmail.com>
2026-02-12 22:20:23 +0530

    Revert "Add authentication system with Firebase, login and register pages"

--- app/page.js
+++ app/page.js
@@ -19,18 +19,12 @@ export default function Home() {
       </h1>
 
       {!loggedIn && (
-        <div className="mb-6 space-x-4">
+        <div className="mb-6">
           <Link
             href="/login"
             className="text-indigo-400 hover:underline text-sm"
           >
             Login
-          </Link>
-          <Link
-            href="/register"
-            className="text-indigo-400 hover:underline text-sm"
-          >
-            Register
           </Link>
         </div>
       )}
